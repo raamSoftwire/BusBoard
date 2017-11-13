6d6861a09fbfe3ffc3a09bb998350a9e0a29902f
Raam Chauhan <Raam.Chauhan@softwire.com>
2017-11-13 11:31:59 +0000

    refactor, rename

--- busHelper.js
+++ busHelper.js
@@ -6,7 +6,6 @@ function getArrivalTime(obj)
 {
     const unixTime = moment(obj.expectedArrival,"YYYY-MM-DDTHH:mm:ssZ",'en').valueOf();
     return unixTime;
-
 }
 
 function getTimeTilArrival(obj)
@@ -14,7 +13,28 @@ function getTimeTilArrival(obj)
     const expArr = moment(obj.expectedArrival,"YYYY-MM-DDTHH:mm:ssZ",'en');
     const timeTilArrival = expArr.fromNow();
     return timeTilArrival;
+}
 
+function sortByDistance(stopPoints)
+{
+    return stopPoints.sort(function(a,b)
+    {
+        return a.distance - b.distance;
+    })
+}
+
+function sortByArrivalTime(busDataArray)
+{
+    return busDataArray.sort(function(a,b)
+    {
+        return getArrivalTime(a) - getArrivalTime(b)
+    })
+}
+
+function makeStopDetailsRequestString(stopId)
+{
+    return 'https://api.tfl.gov.uk/StopPoint/' + stopId
+        + '/Arrivals?app_id=72ae2528&app_key=e51178ced390783e1bc24c32fe85b8d1';
 }
 
 function displayBusTimes(obj)
@@ -32,28 +52,6 @@ function displayBusTimes(obj)
         + timeTilArrival);
 }
 
-function sortByDistance(stopPoints)
-{
-    return stopPoints.sort(function(a,b)
-    {
-        return a.distance - b.distance;
-    })
-}
-
-function sortByArrivalTime(busDataArray) {
-
-    return busDataArray.sort(function(a,b)
-        {
-            return getArrivalTime(a) - getArrivalTime(b)
-        })
-}
-
-function makeStopDetailsRequestString(stopId)
-{
-    return 'https://api.tfl.gov.uk/StopPoint/' + stopId
-        + '/Arrivals?app_id=72ae2528&app_key=e51178ced390783e1bc24c32fe85b8d1';
-}
-
 function displayHelper(buses)
 {
     console.log('\n' + buses[0].stationName + "\n");
@@ -68,10 +66,6 @@ function displayBusData(stopId)
         .then(busDataArray => sortByArrivalTime(busDataArray))
         .then(busDataArray => busDataArray.slice(0,5))
         .then(displayHelper)
-
-
-
 }
 
-module.exports = {getArrivalTime, displayBusTimes, sortByDistance,
-    sortByArrivalTime, displayBusData};
+module.exports = {displayBusTimes, sortByDistance, displayBusData};
--- index.js
+++ index.js
@@ -1,74 +1,36 @@
-const request = require('request');
 const readlineSync = require('readline-sync');
-const moment = require('moment');
 const busHelper = require('./busHelper');
 const rp = require('request-promise-native');
 
 
-const postcode = readlineSync.question('Please enter a postcode : ');
-const postcodeRequestString = 'https://api.postcodes.io/postcodes/' + postcode;
+const postcode = 'NW5 1TL';//readlineSync.question('Please enter a postcode : ');
 
-function initialise() {
-    request(postcodeRequestString, postcodeResponseHandler);
+
+function getCoordinatesFromPostcode(postcodeString)
+{
+    const postcodeRequestString = 'https://api.postcodes.io/postcodes/' + postcodeString;
+
+    return rp(postcodeRequestString)
+        .then(JSON.parse)
+        .then(obj => [obj.result.latitude, obj.result.longitude])
 }
 
-function postcodeResponseHandler(error, response, body)
-{
-    obj = JSON.parse(body);
-    const lat = obj.result.latitude;
-    const lon = obj.result.longitude;
 
+function getBusStopIDsFromCoordinates(coordinates)
+{
     busStopRequestString = 'https://api.tfl.gov.uk/StopPoint?stopTypes=%20NaptanPublicBusCoachTram' +
         '&radius=400&useStopPointHierarchy=false&modes=bus&returnLines=false&lat='
-        + lat +'&lon=' + lon;
-
-    request(busStopRequestString,busStopResponseHandler);
+        + coordinates[0] +'&lon=' + coordinates[1];
+    return rp(busStopRequestString)
+        .then(JSON.parse)
+        .then(obj => obj.stopPoints)
+        .then(stopPoints => busHelper.sortByDistance(stopPoints))
+        .then(stopPoints => stopPoints.slice(0,2))
+        .then(stopPoints => stopPoints.map(stopPoint => stopPoint['naptanId'])
+        )
 }
 
-function busStopResponseHandler(error,response,body)
-{
-    obj = JSON.parse(body);
-    const stopPoints = obj.stopPoints;
 
-    stopPoints.sort(function(a,b)
-    {
-        return a.distance - b.distance;
-    });
-
-    for (i = 0; i < 2 ; i++)
-    {
-        requestString = 'https://api.tfl.gov.uk/StopPoint/' + stopPoints[i].naptanId
-            + '/Arrivals?app_id=72ae2528&app_key=e51178ced390783e1bc24c32fe85b8d1';
-
-        request(requestString, responseHandler);
-    }
-
-}
-
-function responseHandler (error, response, body)
-{
-    const data = JSON.parse(body);
-    data.sort(function(a,b){
-        return busHelper.getArrivalTime(a) - busHelper.getArrivalTime(b)
-    });
-
-    console.log(data[0].stationName + "\n");
-
-    for(i = 0; i < 5; i++)
-        busHelper.displayBusTimes(data[i]);
-
-    console.log("\n");
-}
-
-initialise();
-
-
-// getCoordinatesForPostcode(postcode)
-//     .then(coordinates => getBusStopsForCoordinates(coordinates))
-//     .then(busStops => Promise.all(busStops.map(busStop => getArrivalsForBusStop(busStop))))
-//     .then(arrivals => console.log(arrivals)); // this looks bad
-
-
-
-
-
+getCoordinatesFromPostcode(postcode)
+    .then(coordinates => getBusStopIDsFromCoordinates(coordinates))
+    .then(BusStopIds => BusStopIds.map(busHelper.displayBusData));
