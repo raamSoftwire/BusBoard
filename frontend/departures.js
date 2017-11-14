7c1ec0d035cf5e75dbac935116c76fb7cbf7206d
Raam Chauhan <Raam.Chauhan@softwire.com>
2017-11-14 09:57:23 +0000

    Creating UL from JSON

--- frontend/departures.js
+++ frontend/departures.js
@@ -9,14 +9,57 @@ function functionOnClick(form)
     xhttp.send();
 }
 
+function makeUL(myArray) {
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    for (var i = 0; i < myArray.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+
+        // Set its contents:
+        item.appendChild(document.createTextNode(myArray[i]));
+
+        // Add it to the list:
+        list.appendChild(item);
+    }
+    return list;
+}
+
+
+
 
 xhttp.onload = function() {
     obj = JSON.parse(xhttp.response);
-    document.getElementById('stop1Header').innerHTML = obj[0][0].stationName;
-    document.getElementById('stop2Header').innerHTML = obj[1][0].stationName;
+    document.getElementById('stopHeader0').innerHTML = obj[0][0].stationName;
+    document.getElementById('stopHeader1').innerHTML = obj[1][0].stationName;
+
+    for(i=0; i<2; i++)
+    {
+        stopList = document.getElementById('stopList'+ i);
+
+        while ( stopList.hasChildNodes()) {
+            stopList.removeChild(stopList.firstChild);
+        }
+        stringArray = [];
+        for(j =0; j<5; j++)
+        {
+            str = "Time to Station " + obj[i][j].timeToStation + " seconds";
+            stringArray.push(str);
+        }
+
+        stopList.appendChild(makeUL(stringArray));
+    }
+
     console.log(xhttp.response);
     // Handle response here using e.g. xhttp.status, xhttp.response, xhttp.responseText
 };
+
+
+
+
+
+
 
 
 
